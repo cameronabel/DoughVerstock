ab592b965a30033abbfca4de4d5f4fb1adb40017
Cameron Abel <cameronabel@gmail.com>
2023-02-22 23:09:59 -0800

    recycle order code and tests

--- DoughVerstock.Tests/ModelTests/OrderTests.cs
+++ DoughVerstock.Tests/ModelTests/OrderTests.cs
@@ -2,10 +2,101 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DoughVerstock.Models;
 using System;
 
-namespace DoughVerstock.Tests
+namespace DoughVerstock.Tests;
+
+[TestClass]
+public class OrderTests : IDisposable
 {
-  [TestClass]
-  public class OrderTests
+  public void Dispose()
   {
+    Order.ClearAll();
+  }
+
+  [TestMethod]
+  public void OrderConstructor_CreatesInstanceOfOrder_Order()
+  {
+    Order testOrder = new Order();
+    Assert.AreEqual(typeof(Order), testOrder.GetType());
+  }
+  [TestMethod]
+  public void OrderConstructor_Assigns_NextOrderNumber_Int()
+  {
+    Order testOrder = new Order();
+    Assert.AreEqual(1, testOrder.ID);
+  }
+  [TestMethod]
+  public void AddGood_AddsGoodToCart_UpdatedCart()
+  {
+    Order testOrder = new Order();
+    Bread testBread = new Bread(1);
+    testOrder.AddGood(testBread);
+    Assert.AreEqual(1, testOrder.Cart.Count);
+  }
+  [TestMethod]
+  public void StringReceipt_ReturnsStringReceipt_SingleGood()
+  {
+    Order testOrder = new Order();
+    Bread testBread = new Bread(1);
+    testOrder.AddGood(testBread);
+    string expectedReceipt = $"    {"Bread",32}    {"5",4}    \n    {new string('─', 40)}    \n{"TOTAL: 5",44}    ";
+    Assert.AreEqual(expectedReceipt, testOrder.StringReceipt());
+  }
+  [TestMethod]
+  public void StringReceipt_ReturnsStringReceipt_MultipleGoods()
+  {
+    Order testOrder = new Order();
+    Bread testBread = new Bread(1);
+    Pastry testPastries = new Pastry(4);
+    testOrder.AddGood(testBread);
+    testOrder.AddGood(testPastries);
+    string expectedReceipt = $"    {"Bread",32}    {"5",4}    \n    {" 4 x Pastry @ 2 ea B3G1 DEAL",32}    {"6",4}    \n    {new string('─', 40)}    \n{"TOTAL: 11",44}    ";
+    Assert.AreEqual(expectedReceipt, testOrder.StringReceipt());
+  }
+  [TestMethod]
+  public void StringReceipt_ReturnsStringReceipt_MultipleLines()
+  {
+    Order testOrder = new Order();
+    Bread testBread = new Bread(1);
+    Pastry testPastries = new Pastry(5);
+    testOrder.AddGood(testBread);
+    testOrder.AddGood(testPastries);
+    string expectedReceipt = $"    {"Bread",32}    {"5",4}    \n    {" 4 x Pastry @ 2 ea B3G1 DEAL",32}    {"6",4}    \n    {"Pastry",32}    {"2",4}    \n    {new string('─', 40)}    \n{"TOTAL: 13",44}    ";
+    Assert.AreEqual(expectedReceipt, testOrder.StringReceipt());
+  }
+  [TestMethod]
+  public void ConfirmPrice_ProperlyCalculatesPrice_RegularPrice()
+  {
+    Order testOrder = new Order();
+    Bread testBread = new Bread(2);
+    testOrder.AddGood(testBread);
+    string expectedReceipt = $"    {"Bread",32}    {"5",4}    \n    {"Bread",32}    {"5",4}    \n    {new string('─', 40)}    \n{"TOTAL: 10",44}    ";
+    Assert.AreEqual(expectedReceipt, testOrder.StringReceipt());
+  }
+  [TestMethod]
+  public void ConfirmPrice_ProperlyCalculatesPrice_BOGOPrice()
+  {
+    Order testOrder = new Order();
+    Bread testBread = new Bread(2);
+    Bread thirdBread = new Bread();
+    testOrder.AddGood(testBread);
+    testOrder.AddGood(thirdBread);
+    string expectedReceipt = $"    {"Bread",32}    {"5",4}    \n    {"Bread",32}    {"5",4}    \n    {"Bread B2G1 DEAL",32}    {"0",4}    \n    {new string('─', 40)}    \n{"TOTAL: 10",44}    ";
+    Assert.AreEqual(expectedReceipt, testOrder.StringReceipt());
+  }
+  [TestMethod]
+  public void GetTotalPrice_EmptyOrderCostsZero_Zero()
+  {
+    Order testOrder = new Order();
+    Assert.AreEqual(0, testOrder.GetTotalPrice());
+  }
+  [TestMethod]
+  public void GetTotalPrice_TestOrderCalculatesCost_23()
+  {
+    Order testOrder = new Order();
+    testOrder.AddGood(new Bread(3));
+    testOrder.AddGood(new Bread());
+    testOrder.AddGood(new Pastry(4));
+    testOrder.AddGood(new Pastry());
+    Assert.AreEqual(23, testOrder.GetTotalPrice());
   }
 }
--- DoughVerstock/Models/Order.cs
+++ DoughVerstock/Models/Order.cs
@@ -1,10 +1,81 @@
-namespace DoughVerstock.Models
-{
-  public class Order
-  {
-    public Order()
-    {
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
+namespace DoughVerstock.Models;
+
+public class Order
+{
+  public List<Good> Cart { get; set; }
+  public int ID { get; }
+
+  private static int _runningCounter = new int();
+
+  public static void ClearAll()
+  {
+    _runningCounter = 0;
+  }
+  private int ConfirmPrice(Good good)
+  {
+    int tally = Cart.Select(x => x.Quantity).Sum();
+    int mod = tally % (good.BXGO + 1);
+    if (mod == good.BXGO)
+    {
+      return 0;
     }
+    else
+    {
+      return good.Price;
+    }
+  }
+  public int GetTotalPrice() => Cart.Select(good => good.Price).Sum();
+  public void AddGood(dynamic good)
+  {
+    if (good.Quantity == 1)
+    {
+      good.Price = ConfirmPrice(good);
+      if (good.Price == 0)
+      {
+        good.Label += $" B{good.BXGO}G1 DEAL";
+      }
+      Cart.Add(good);
+    }
+    else
+    {
+      int div = good.Quantity / (good.BXGO + 1);
+      int mod = good.Quantity % (good.BXGO + 1);
+      if (div > 0)
+      {
+        good.Quantity = div * (good.BXGO + 1);
+        good.MultiLabel();
+        good.MultiPrice();
+        Cart.Add(good);
+      }
+      Type T = good.GetType();
+      for (int i = 0; i < mod; i++)
+      {
+        Object newGood = Activator.CreateInstance(T);
+        AddGood(newGood);
+      }
+    }
+  }
+  public string StringReceipt()
+  {
+    string receipt = "";
+    int total = 0;
+    foreach (Good good in Cart)
+    {
+      receipt += $"    {good.Label,32}    {good.Price,4}    \n";
+      total += good.Price;
+    }
+    receipt += $"    {new string('─', 40)}    \n";
+    receipt += $"{"TOTAL: " + total.ToString(),44}    ";
+    return receipt;
+  }
+  public Order()
+  {
+    _runningCounter++;
+    ID = _runningCounter;
+    Cart = new List<Good>() { };
   }
 }
