a4c62a697a6f5e62d54ec723eccb48c535011936
Cameron Abel <cameronabel@gmail.com>
2023-02-22 23:04:11 -0800

    add good tests for Croissants

--- DoughVerstock.Tests/ModelTests/CroissantTests.cs
+++ DoughVerstock.Tests/ModelTests/CroissantTests.cs
@@ -1,11 +1,50 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DoughVerstock.Models;
-using System;
 
-namespace DoughVerstock.Tests
+namespace DoughVerstock.Tests;
+
+[TestClass]
+public class CroissantTests
 {
-  [TestClass]
-  public class CroissantTests
+  [TestMethod]
+  public void CroissantConstructor_CreatesInstanceOfCroissant_Croissant()
   {
+    Croissant testCroissant = new Croissant(1);
+    Assert.AreEqual(typeof(Croissant), testCroissant.GetType());
+  }
+  [TestMethod]
+  public void CroissantConstructor_AssignsLabelField_Croissant()
+  {
+    Croissant testCroissant = new Croissant(1);
+    string result = testCroissant.Label;
+    Assert.AreEqual("Croissant", result);
+  }
+  [TestMethod]
+  public void CroissantConstructor_AssignsPriceField_Int()
+  {
+    Croissant testCroissant = new Croissant(1);
+    int result = testCroissant.Price;
+    Assert.AreEqual(3, result);
+  }
+  [TestMethod]
+  public void CroissantConstructor_AssignsQuantityField_Int()
+  {
+    Croissant testCroissants = new Croissant(1);
+    int result = testCroissants.Quantity;
+    Assert.AreEqual(1, result);
+  }
+  [TestMethod]
+  public void CroissantConstructor_AssignsMultiPriceField_Int()
+  {
+    Croissant testCroissants = new Croissant(5);
+    int result = testCroissants.Price;
+    Assert.AreEqual(12, result);
+  }
+  [TestMethod]
+  public void CroissantConstructor_AssignsMultiLabelField_String()
+  {
+    Croissant testCroissants = new Croissant(5);
+    string result = testCroissants.Label;
+    Assert.AreEqual(@" 5 x Croissant @ 3 ea B4G1 DEAL", result);
   }
 }
--- DoughVerstock/Models/Croissant.cs
+++ DoughVerstock/Models/Croissant.cs
@@ -4,10 +4,10 @@ public class Croissant : Good
 {
   public Croissant() : this(1) { }
   public Croissant(int quantity)
-    : base("Croissant", 5)
+    : base("Croissant", 3)
   {
     Quantity = quantity;
-    BXGO = 2;
+    BXGO = 4;
     if (Quantity > 1) { MultiLabel(); MultiPrice(); }
   }
 }
